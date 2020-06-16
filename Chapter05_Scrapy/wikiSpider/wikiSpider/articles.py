bf38bb7082726d9129b3806fa604c5345e0f0c87
Rahul Singh <rahuls0599@gmail.com>
2020-06-16 16:05:14 +0530

    Scraping different sorts of articles using Scrapy

--- Chapter05_Scrapy/wikiSpider/wikiSpider/articles.py
+++ Chapter05_Scrapy/wikiSpider/wikiSpider/articles.py
@@ -17,3 +17,18 @@ class ArticleSpider(CrawlSpider):
         print('title is: {} '.format(title))
         print('text is: {}'.format(text))
         print('Last updated: {}'.format(lastUpdated))
+
+'''This new ArticleSpider extends the CrawlSpider class. Rather than providing a
+start_requests function, it provides a list of start_urls and allowed_domains.
+This tells the spider where to start crawling from and whether it should follow or
+ignore a link based on the domain.
+A list of rules is also provided. This provides further instructions on which links to
+follow or ignore (in this case, you are allowing all URLs with the regular expres‐
+sion .*).
+In addition to extracting the title and URL on each page, a couple of new items have
+been added. The text content of each page is extracted using an XPath selector. XPath
+is often used when retrieving text content including text in child tags (for example, an
+<a> tag inside a block of text). If you use the CSS selector to do this, all text within
+child tags will be ignored.
+The last updated date string is also parsed from the page footer and stored in the
+lastUpdated variable.'''
